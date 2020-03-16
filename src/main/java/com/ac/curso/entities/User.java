18399f27a55ab535e4a109d3bdc99a57729cb7f8
limaantonio <limaantoniocarlos20@gmail.com>
2020-03-16 13:41:53 -0300

    Service layer, component registration, part 1

--- src/main/java/com/ac/curso/entities/User.java
+++ src/main/java/com/ac/curso/entities/User.java
@@ -1,13 +1,20 @@
 package com.ac.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import ch.qos.logback.core.net.server.Client;
 
 @Entity
+@Table(name = "tbUser")
 public class User implements Serializable{
 	
 	/**
@@ -22,6 +29,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 
@@ -48,7 +58,7 @@ public class User implements Serializable{
 		this.id = id;
 	}
 
-
+	
 
 	public String getName() {
 		return name;
@@ -102,6 +112,7 @@ public class User implements Serializable{
 		return serialVersionUID;
 	}
 
+	
 
 
 	@Override
@@ -129,6 +140,12 @@ public class User implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
--- src/main/java/com/ac/curso/respositories/UserRespository.java
+++ src/main/java/com/ac/curso/respositories/UserRespository.java
@@ -1,8 +1,10 @@
 package com.ac.curso.respositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ac.curso.entities.User;
+
 
 public interface UserRespository extends JpaRepository<User, Long>{
 
